3f9cad595ba13f580c38713873511849812aead0
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-12 14:56:03 +0100

    add function for userprofile/[user_id]

--- pages/api/userprofile/[user_id].js
+++ pages/api/userprofile/[user_id].js
@@ -1,5 +1,9 @@
-SELECT u.*, v.*
-FROM user u
-JOIN booking b ON u.user_id = b.user_id
-JOIN vehicle v ON b.vehicle_id = v.vehicle_id
-WHERE u.user_id = {user_id};
+import database from "../../../../database.json"; //import database
+
+export default function getAllUsers(req, res) {
+  const query =
+    "SELECT u.*, v.* FROM user u JOIN booking b ON u.user_id = b.user_id JOIN vehicle v ON b.vehicle_id = v.vehicle_id WHERE u.user_id = {user_id};";
+  const { user_id } = req.query;
+
+  res.json(database.find((user) => user.user_id === +user_id));
+}
