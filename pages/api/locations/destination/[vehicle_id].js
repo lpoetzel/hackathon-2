3d41fa14bc5e93b5858524622238478aaef9456f
MarianneH1 <marianne.helbig@googlemail.com>
2023-01-13 09:17:54 +0100

    fix destination query

--- pages/api/locations/destination/[vehicle_id].js
+++ pages/api/locations/destination/[vehicle_id].js
@@ -3,8 +3,10 @@ import connection from "../../auth/lib/db";
 export default async function handler(req, res) {
   const { vehicle_id } = req.query;
   const query =
-    "SELECT DISTINCT location.venue_name FROM location JOIN booking ON booking.destination=location.location_id JOIN vehicle ON vehicle.vehicle_id=booking.vehicle_id WHERE vehicle.vehicle_id=" +
-    vehicle_id;
+    "SELECT DISTINCT location.venue_name, booking.booking_id FROM location JOIN booking ON booking.destination=location.location_id JOIN vehicle ON vehicle.vehicle_id=booking.vehicle_id WHERE vehicle.vehicle_id=" +
+    vehicle_id +
+    " ORDER BY booking.booking_id DESC LIMIT 1;";
+  console.log(query);
   connection
     .promise()
     .query(query)
